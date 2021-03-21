e6c6982cc021e14d9edd2167b4b9708ad54e1595
sho <sho02.soccer@gmail.com>
2021-03-21 23:23:09 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8')
+);
+
+module.exports = {
+  plugins: ['prettier'],
+  extends: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+  },
+};
